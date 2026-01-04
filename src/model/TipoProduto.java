a035a52fb1554a252f329237c07f1ef295b9d727
Rodrigo Macedo <119683555+rodrigomcd2@users.noreply.github.com>
2026-01-04 11:16:22 -0300

    Create TipoProduto.java
    Classe que mexe com os tipos

---
+++ src/model/TipoProduto.java
@@ -0,0 +1,10 @@
+package model;
+
+public final class TipoProduto {
+
+    public static final String MICROCONTROLADOR = "Microcontrolador";
+    public static final String PROTOTIPAGEM = "Prototipagem";
+    public static final String SENSOR = "Sensor";
+
+    private TipoProduto() {}
+}
