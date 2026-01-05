f1ad509fa1e40fc5462fd421a998523456b6ecdc
Rodrigo Macedo <119683555+rodrigomcd2@users.noreply.github.com>
2026-01-05 18:56:27 -0300

    Update Main.java

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,13 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+package app;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+import view.*;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+
+    public static void main(String[] args) {
+    // Cria a tela
+        SwingUtilities.invokeLater(() -> {
+
+            VitrineView vitrineView = new VitrineView();// recebe as atualizações de Vitrine
+            FormProdutoView formProdutoView = new FormProdutoView(vitrineView);
+            HistoricoView historicoView = new HistoricoView();
+
+            DashboardView dashboard = new DashboardView(
+                    vitrineView,
+                    formProdutoView,
+                    null,
+                    historicoView
+            );
+
+            AreaVendedorView areaVendedorView =
+                    new AreaVendedorView(dashboard);
+
+            dashboard.setAreaVendedorView(areaVendedorView);
+
+            dashboard.setVisible(true);
+        });
     }
 }
